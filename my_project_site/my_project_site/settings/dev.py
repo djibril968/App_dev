807d868f20e411d28c0bbe1ba4e21e9f77e3c6ba
djibril968 <djibril.lekan@gmail.com>
2024-08-16 00:34:56 +0300

    initial commit

---
+++ my_project_site/my_project_site/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-1&+o51to#lvz@3d4%f(^v1d_vc_*^v+e44)g@6jc)med4tq3y^"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
